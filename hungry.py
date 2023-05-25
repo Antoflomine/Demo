cef15670363f569346ac970acda6ec4b768debab
Antoflomine <68322807+Antoflomine@users.noreply.github.com>
2023-05-25 12:49:46 +0530

    fries

--- hungry.py
+++ hungry.py
@@ -2,6 +2,7 @@ hungry=input("Are you hungry?")
 if hungry == "yes":
 	print("Eat pizza")
 	print("Eat samosa")
+	print("Eat fries")
 else:
 	if thirsty == "yes":
 		print("Drink water")
@@ -9,5 +10,4 @@ else:
 =======
 		print("Drink juice")
 >>>>>>> thirsty
-
 		
